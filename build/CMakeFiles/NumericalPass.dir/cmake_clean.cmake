c3e4c37a63b3318a73a46fc3e2df3c0d3534330d
Your Name <you@example.com>
2026-05-31 23:52:07 +0530

    restructured the repo

---
+++ build/CMakeFiles/NumericalPass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumericalPass.dir/src/NumericalPass.cpp.o"
+  "CMakeFiles/NumericalPass.dir/src/NumericalPass.cpp.o.d"
+  "NumericalPass.pdb"
+  "NumericalPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NumericalPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
